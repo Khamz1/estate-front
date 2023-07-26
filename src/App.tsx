5b59a1da45f1fed8daba30446644b9cd2d9b90c0
Djamalayl <djamalayl32@mail.ru>
2023-07-26 15:43:25 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import Estate from "./components/Estates/Estate"
+import Header from "./components/Header/Header"
 
 
 function App() {
 
   return (
     <>
-  
+  <Header/>
       <Estate/>
      
     </>
