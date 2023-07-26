51a18914a00bc5b0e2c85ad154d6cbd871a232d0
Yunusov <abubakar.yunusov.47@mail.ru>
2023-07-26 22:28:21 +0300

    Cart update

--- src/components/Estates/Estate.tsx
+++ src/components/Estates/Estate.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEstates } from "../../features/estate.slice";
 import styles from './Estate.module.scss'
-import img from '../../acces/icons/Без названия.jpg'
+import img from '../../acces/icons/testCart-fotor-bg-remover-20230726182712.png'
 
 import { HiOutlineHeart} from "react-icons/hi"; 
 
@@ -21,23 +21,17 @@ function Estate() {
 
 
   return (
-    <div>
-      
+    <div>    
       <div className={styles.box}>
-      {estates.map((item)=>{
-        return(
-          
             <div className={styles.card}>
-              <div className={styles.type}>{item.type} <HiOutlineHeart style={{ fontSize: 30 }} /></div>
+              <div className={styles.roomsCount} >2-ком. планировка</div>
+              <div className={styles.type}><HiOutlineHeart style={{ fontSize: 24 }} /></div>
               <div className={styles.image}><img src={img} alt="img" /></div>
               <div className={styles.address}>
-              <div>{item.area}</div>
-              <div>{item.address}</div>
+              <div className={styles.square}>67.5 кв/м <br/>Нижний этаж</div>
               </div>
               <button className={styles.button}>Заказать</button>
             </div>
-        )
-     })}
      </div>
     </div>
   );
