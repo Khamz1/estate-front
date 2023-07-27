e00c9a1ee05ae1e448f5ecad09a5c61cecfb6ad2
Adam-IT <Alan_Salons@mail.ru>
2023-07-27 11:39:13 +0300

    добавил auth, добавил некоторые route в app,сделал  отдельную папку types  для удобство типизации

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-
 import { Routes, Route } from "react-router-dom";
 import Estate from "./components/Estates/Estate";
 import Auth from "./pages/Authorization/Auth";
 import Login from "./pages/Authorization/Login";
 
-
-
-import Header from "./components/Header/Header"
-import './App.css'
-
+import Header from "./components/Header/Header";
+import "./App.css";
 
 function App() {
   return (
-
     <>
+      <div className="container">
+        <Header />
+      </div>
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/login" element={<Login />} />
@@ -21,14 +19,6 @@ function App() {
       </Routes>
     </>
   );
-
-    <div className="container">
-  <Header/>
-      {/* <Estate/> */}
-     
-    </div>
-  )
-
 }
 
 export default App;
