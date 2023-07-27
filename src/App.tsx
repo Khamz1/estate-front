86954ab00314ba4c1fa70ab051bc1d10d66e0166
Adam-IT <Alan_Salons@mail.ru>
2023-07-27 11:02:07 +0300

    добавил auth, добавил некоторые route в app,сделал  отдельную папку types  для удобство типизации

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-
-import Estate from "./components/Estates/Estate"
-
+import { Routes, Route } from "react-router-dom";
+import Estate from "./components/Estates/Estate";
+import Auth from "./pages/Authorization/Auth";
+import Login from "./pages/Authorization/Login";
 
 function App() {
-
   return (
     <>
-  
-      <Estate/>
-     
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Estate />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import estates from '../features/estate.slice'
+import auth from '../features/authSlice'
+import login from '../features/loginSlice'
 export const store = configureStore({
     reducer:{
-    estates
+    estates,
+    auth,
+    login
     }
 })
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
